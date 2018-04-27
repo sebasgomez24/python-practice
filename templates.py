6f4242255058aa673f0fa79ce61f0a4641bc0d3b
sebasgomez24 <sebasgomez24@gmail.com>
2018-04-27 15:22:14 -0700

    changes

--- templates.py
+++ templates.py
@@ -1,7 +1,7 @@
 import os
 
-
-
 def get_template_path(path):
     file_path = os.path.join(os.getcwd(), path)
+    if now os.path.isfile(file_path):
+        raise Exception("This is not a valid template path")
     return file_path
